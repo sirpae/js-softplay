8513205f8fecc33e591c28d04609a6e61e7ec139
p <101421945+sirpae@users.noreply.github.com>
2022-05-23 23:24:10 +0100

    soft play draft

--- src/soft-play.js
+++ src/soft-play.js
@@ -11,10 +11,37 @@ let children = 0
 // TODO: Write your functions in the below section. Your functions should update
 // the adults and children variables defined above.
 
+function enter(numAdults, numChildren) {
+  if (numAdults >= numChildren) {
+    adults += numAdults
+    children += numChildren
+    return true
+  }
+
+  return false
+}
+
+function leave(numAdults, numChildren) {
+  if (numChildren > numAdults || (adults - numAdults) < (children - numChildren)) {
+    return false
+  }
+
+  adults -= numAdults
+  children -= numChildren
+  return true
+}
+
+function occupancy () {
+  return {
+    adults: adults,
+    children: children
+  }
+}
+
 // TODO: Change the undefined values below to the name of your functions
 module.exports = {
-  enter: undefined,
-  leave: undefined,
-  occupancy: undefined,
+  enter: enter,
+  leave: leave,
+  occupancy: occupancy,
   reset: reset
 }
